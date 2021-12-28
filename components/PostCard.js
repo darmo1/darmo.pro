e67bb4be3209ad59f14a960ef25bdca6830d7b62
Danilo <darmito10@gmail.com>
2021-12-28 02:29:34 -0500

    add color to cards blogs

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Link from 'next/link'
 
+function randomColor(){
+  const randomNumber = Math.random();
+  const bgColors = ['bg-purple-300', 'bg-pink-300', 'bg-amber-300', 'bg-orange-300', 'bg-esmerald-300', 'bg-cyan-300', 'bg-indigo-400']
+  const index = Math.floor(randomNumber * bgColors.length)
+  return bgColors[index]
+}
+
 const PostCard = ({post}) => {
   return (
     <Link href={`blog/${post.slug}`} key={post.slug}>
       <a>
-        <div className="w-40 rounded h-48 my-4 mx-2 border-2">
+        <div className={`w-40 rounded h-48 my-4 mx-2 border-2 p-2 ${randomColor()}`}>
           <h4 className="font-medium text-gray-900 dark:text-gray-100 my-1">
             {post.frontmatter?.title}
           </h4>
-          <p className="text-gray-700 dark:text-gray-400 ">Hola como estás</p>
         </div>
       </a>
     </Link>
